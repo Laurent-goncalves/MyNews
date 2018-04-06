5634c6f8963e0b670049306d18b0c17c36ebc094
Laurent <laurentgoncalves@outlook.com>
2018-04-06 22:58:23 +0200

    Finalization of toolbar and creation of settingActivity

--- app/src/main/java/com/g/laurent/mynews/Controllers/MainActivity.java
+++ app/src/main/java/com/g/laurent/mynews/Controllers/MainActivity.java
@@ -1,26 +1,69 @@
 package com.g.laurent.mynews.Controllers;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.view.menu.MenuBuilder;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.g.laurent.mynews.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar_menu_button) ImageButton icon_menu;
+    @BindView(R.id.toolbar_menu_search) ImageButton icon_search;
     private MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setIconOnClickListener();
         this.configureAndShowMainFragment();
         this.configureToolBar();
         this.configureTabLayout();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Toast toast;
+
+        switch (item.getItemId()){
+            case R.id.notifications:
+                toast = Toast.makeText(this,"Item notification selected",Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.help:
+                toast = Toast.makeText(this,"Item help selected",Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.about:
+                toast = Toast.makeText(this,"Item about selected",Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // -------------- CONFIGURATION --------------------
 
     private void configureAndShowMainFragment(){
 
@@ -56,4 +99,25 @@ public class MainActivity extends AppCompatActivity {
                 tablayout.addTab(tablayout.newTab().setText(tab));
         }
     }
+
+    private void setIconOnClickListener(){
+
+        icon_menu.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        icon_search.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                startSettingActivity();
+            }
+        });
+    }
+
+    private void startSettingActivity(){
+        Intent intent = new Intent(this,SettingActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/g/laurent/mynews/Controllers/SettingActivity.java
@@ -0,0 +1,19 @@
+package com.g.laurent.mynews.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.g.laurent.mynews.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+
+
+
+}
